72e3e6ef2e9daafcbc33d458ac1ed0034eb805e2
Dmitry_Klus <Dmitry_Klus@DESKTOP-ACO831Q>
2024-05-24 23:55:28 +1000

    2321

---
+++ Intermediate/Build/Win64/SnakeGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.40.33808
+#include "C:/Users/Dmitry_Klus/Documents/Unreal Projects/SnakeGame/Intermediate/Build/Win64/SnakeGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
